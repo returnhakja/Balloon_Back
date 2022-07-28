6a5df779f9bf383b9181a13c03170b1535ab9425
UIJIN <62687865+kki45@users.noreply.github.com>
2022-07-28 16:27:01 +0900

    chore : basic-setting

---
+++ balloon/src/main/java/com/balloon/demo/BalloonApplication.java
@@ -0,0 +1,13 @@
+package com.balloon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalloonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalloonApplication.class, args);
+	}
+
+}
