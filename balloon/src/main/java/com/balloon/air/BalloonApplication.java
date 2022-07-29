96f0fd4e7e343d3dbafd80ef9aecd988d24c7cf5
Uno0306 <82020768+Uno0306@users.noreply.github.com>
2022-07-29 10:27:04 +0900

    Add annotations to the BalloonApplication

---
+++ balloon/src/main/java/com/balloon/air/BalloonApplication.java
@@ -0,0 +1,21 @@
+package com.balloon.air;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EntityScan(basePackages = {"com.balloon.entity"})
+@EnableJpaRepositories(basePackages = {"com.balloon.repository"})
+@ComponentScan(basePackages = {"com.balloon.exception", "com.balloon.service", "com.balloon.controller", "com.balloon.dto"})
+public class BalloonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalloonApplication.class, args);
+	}
+
+}
