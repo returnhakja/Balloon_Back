2a9f5602af85cfa86119e121a5c77424406bfedc
UIJIN <62687865+kki45@users.noreply.github.com>
2022-08-12 18:57:18 +0900

    chatting update

--- balloon/src/main/java/com/balloon/service/ChatREmpServiceImpl.java
+++ balloon/src/main/java/com/balloon/service/ChatREmpServiceImpl.java
@@ -1,5 +1,6 @@
 package com.balloon.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -9,12 +10,10 @@ import org.springframework.stereotype.Service;
 import com.balloon.dto.ChatroomDTO;
 import com.balloon.dto.ChatroomEmployeeDTO;
 import com.balloon.dto.EmpDTO;
-import com.balloon.entity.Chatroom;
 import com.balloon.entity.ChatroomEmployee;
 import com.balloon.entity.ChatroomEmployeeId;
 import com.balloon.entity.Employee;
 import com.balloon.repository.ChatREmpRepository;
-import com.balloon.repository.ChatroomRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,12 +22,16 @@ import lombok.RequiredArgsConstructor;
 public class ChatREmpServiceImpl implements ChatREmpService {
 
 	private final ChatREmpRepository chatREmpRepository;
-	private final ChatroomRepository chatroomRepository;
 
 	@Override
 	@Transactional
 	public List<ChatroomEmployee> getallChatEmp() {
 		return chatREmpRepository.findAll();
+	}
+
+	@Override
+	public List<ChatroomEmployee> getChatroomEmp(Long chatroomId) {
+		return chatREmpRepository.findAllByChatroomIdChatroomId(chatroomId);
 	}
 
 	@Override
@@ -39,23 +42,31 @@ public class ChatREmpServiceImpl implements ChatREmpService {
 
 	@Override
 	@Transactional
-	public Employee getInsertChatEmp(ChatroomEmployeeDTO chatroomEmployeeDTO) {
-		ChatroomEmployee chatEmpEntity = chatroomEmployeeDTO.toEntity(chatroomEmployeeDTO);
-		Employee emp = chatREmpRepository.save(chatEmpEntity).getEmpId();
-		if (emp != null) {
-			Chatroom chatroomId = chatroomRepository.findById(chatroomEmployeeDTO.getChatroomId().getChatroomId())
-					.get();
-			// 생성자 패턴 사용할 예정
-			// 생성자 생성 후 처음 chatroomDTO.setChatroomId();만 해줄 시 값은 null임
-			// set메소드로 ChatroomDTO에 있는 id,name,count값을 가져와서 넣어줌
-			ChatroomDTO chatroomDTO = new ChatroomDTO();
-			chatroomDTO.setChatroomId(chatroomId.getChatroomId());
-			chatroomDTO.setChatroomName(chatroomId.getChatroomName());
-			chatroomDTO.setHeadCount(chatroomId.getHeadCount() + 1);
-			// 사람 많이 추가 시 Controller에서 List로 받아옴, List.size()만큼 head_Counter를 추가해준다
-			chatroomRepository.save(chatroomDTO.toEntity(chatroomDTO));
+	public Employee getInsertChatEmp(List<ChatroomEmployeeDTO> chatroomEmployeeDTO) {
+		List<ChatroomEmployee> chatroomEmployeeEntity = new ArrayList<ChatroomEmployee>();
+
+		for (ChatroomEmployeeDTO chatEmpDto : chatroomEmployeeDTO) {
+			chatroomEmployeeEntity.add(chatEmpDto.toEntity(chatEmpDto));
 		}
-		return emp;
+
+		chatREmpRepository.saveAll(chatroomEmployeeEntity);
+
+//		ChatroomEmployee chatEmpEntity = 
+//		Employee emp = chatREmpRepository.save(chatEmpEntity).getEmpId();
+//		if (emp != null) {
+//			Chatroom chatroomId = chatroomRepository.findById(chatroomEmployeeDTO.getChatroomId().getChatroomId())
+//					.get();
+		// 생성자 패턴 사용할 예정
+		// 생성자 생성 후 처음 chatroomDTO.setChatroomId();만 해줄 시 값은 null임
+		// set메소드로 ChatroomDTO에 있는 id,name,count값을 가져와서 넣어줌
+//			ChatroomDTO chatroomDTO = new ChatroomDTO();
+//			chatroomDTO.setChatroomId(chatroomId.getChatroomId());
+//			chatroomDTO.setChatroomName(chatroomId.getChatroomName());
+//			chatroomDTO.setHeadCount(chatroomId.getHeadCount() + 1);
+		// 사람 많이 추가 시 Controller에서 List로 받아옴, List.size()만큼 head_Counter를 추가해준다
+//			chatroomRepository.save(chatroomDTO.toEntity(chatroomDTO));
+//	}return emp;
+		return null;
 	}
 
 	@Override
--- balloon/src/main/java/com/balloon/service/ChatService.java
+++ balloon/src/main/java/com/balloon/service/ChatService.java
@@ -3,16 +3,17 @@ package com.balloon.service;
 import java.util.List;
 
 import com.balloon.dto.ChatDTO;
-import com.balloon.entity.Chat;
 import com.balloon.entity.Employee;
 import com.balloon.vo.MessageDTO;
 
 public interface ChatService {
-	
+
 	public List<ChatDTO> getChat(Employee empId);
-	
+
 	public void insertChat(MessageDTO messageDTO);
 
 	public List<ChatDTO> getChatroomId(Long chatroomId);
+
+//	public void getInsertChat(ChatDTO chatDTO);
 
 }
