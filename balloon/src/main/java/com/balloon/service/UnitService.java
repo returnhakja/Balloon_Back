1fceb87b557da0a920f53574555eff63ef38d93c
Uno0306 <82020768+Uno0306@users.noreply.github.com>
2022-08-16 09:23:40 +0900

    f/l - commit

--- balloon/src/main/java/com/balloon/service/UnitService.java
+++ balloon/src/main/java/com/balloon/service/UnitService.java
@@ -3,17 +3,16 @@ package com.balloon.service;
 import java.util.List;
 
 import com.balloon.dto.UnitDTO;
-import com.balloon.entity.Unit;
 
 public interface UnitService {
 	public List<UnitDTO> findUnitAll();
-	
-	public UnitDTO findUnitByUnitCode(String unitCode);
-	
+
+	public UnitDTO findUnitByUnitCode(String unitCode) throws Exception;
+
 	public void insertUnit(UnitDTO unitDTO);
-	
+
 	public void updateUnit(UnitDTO unitDTO);
-	
+
 	public void deleteUnitByUnitCode(String unitCode);
 
 }
--- balloon/src/main/java/com/balloon/service/UnitServiceImpl.java
+++ balloon/src/main/java/com/balloon/service/UnitServiceImpl.java
@@ -1,5 +1,6 @@
 package com.balloon.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -14,28 +15,28 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class UnitServiceImpl implements UnitService{
-	
+public class UnitServiceImpl implements UnitService {
+
 	private final UnitRepository unitRepo;
-	
+
 	@Transactional
 	@Override
-	public List<UnitDTO> findUnitAll(){
+	public List<UnitDTO> findUnitAll() {
 		List<Unit> unitList = unitRepo.findAll();
 		List<UnitDTO> unitDTOList = new ArrayList<UnitDTO>();
 		unitList.forEach(unitEntity -> unitDTOList.add(unitEntity.toDTO(unitEntity)));
-		
+
 		return unitDTOList;
 	}
 
 	@Transactional
 	@Override
-	public UnitDTO findUnitByUnitCode(String unitCode) {
+	public UnitDTO findUnitByUnitCode(String unitCode) throws Exception {
 		Unit unitEntity = unitRepo.findUnitByUnitCode(unitCode);
 		if (unitEntity == null) {
-				throw new Exception("조직 번호가 존재하지 않습니다.");
-			}
-		UnitDTO unitDTO = unitEntity.toDTO(unitEntity)
+			throw new Exception("조직 번호가 존재하지 않습니다.");
+		}
+		UnitDTO unitDTO = unitEntity.toDTO(unitEntity);
 		return unitDTO;
 	};
 
@@ -43,31 +44,30 @@ public class UnitServiceImpl implements UnitService{
 	@Override
 	public void insertUnit(UnitDTO unitDTO) {
 		Unit unitEntity = unitDTO.toEntity(unitDTO);
-		
+
 		unitRepo.save(unitEntity);
 	};
-	
+
 	@Transactional
 	@Override
 	public void updateUnit(UnitDTO unitDTO) {
 		/* 부서의 모든 정보를 수정할 때 */
 		Unit unitEntity = unitDTO.toEntity(unitDTO);
-		
+
 //		/* 부서 정보의 일부분만 수정할 때 */
 //		Unit unit = findUnitByUnitCode(unitDTO.getUnitCode());
 //		/* 부서전화번호만 수정할 때 */
 //		unit.updateBell(unitDTO);
 //		/* 부서전화번호와 부서명만 수정할 때 */
 //		unit.updateUnitNameAndBell(unitDTO);
-		
+
 		unitRepo.save(unitEntity);
 	};
-	
+
 	@Transactional
 	@Override
 	public void deleteUnitByUnitCode(String unitCode) {
 		unitRepo.deleteUnitByUnitCode(unitCode);
 	};
 
-	
 }
