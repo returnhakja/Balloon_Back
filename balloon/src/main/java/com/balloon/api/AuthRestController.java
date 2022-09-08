ac53be5b5ddb9dc04e124e1e74ee496c4d194846
Uno0306 <82020768+Uno0306@users.noreply.github.com>
2022-09-08 15:48:35 +0900

    Update AuthRestController.java

--- balloon/src/main/java/com/balloon/api/AuthRestController.java
+++ balloon/src/main/java/com/balloon/api/AuthRestController.java
@@ -71,7 +71,6 @@ public class AuthRestController {
 			createCookie.setPath("/");
 			response.addCookie(createCookie);
 		} else {
-//			webSecurityConfig.clearJsession();
 			throw new Exception("토큰 생성 에러입니다.");
 		}
 
