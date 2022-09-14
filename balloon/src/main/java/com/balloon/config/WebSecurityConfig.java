d7431f11d9f702556fb1a6b7f41abc67a4d50b80
도헌's <khs39200@gmail.com>
2022-09-14 15:12:36 +0900

    Feat:DisTribute Back ver-4

--- balloon/src/main/java/com/balloon/config/WebSecurityConfig.java
+++ balloon/src/main/java/com/balloon/config/WebSecurityConfig.java
@@ -74,7 +74,7 @@ public class WebSecurityConfig implements WebMvcConfigurer {
 		registry.addMapping("/**")
 //		.allowedOrigins("http://localhost:3000", "http://15.164.224.26:8080")
 //				.allowedOrigins("http://localhost:3000")
-				.allowedOrigins("http://54.180.148.125:8080")
+				.allowedOrigins("http://54.180.148.125:80")
 				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*")
 				.allowCredentials(true).maxAge(MAX_AGE_SECS);
 	}
