6caf5b0da03937ed51054f0e8003581b1870f5b8
도헌's <khs39200@gmail.com>
2022-09-14 18:40:11 +0900

    Feat:DisTribute Back ver-10

--- balloon/src/main/java/com/balloon/api/AuthRestController.java
+++ balloon/src/main/java/com/balloon/api/AuthRestController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,6 +59,7 @@ public class AuthRestController {
 	}
 
 //	@CrossOrigin(origins = { "http://localhost:3000" })
+	@CrossOrigin(origins = { "http://54.180.148.125:8080" })
 	@PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<TokenDTO> login(@Valid @RequestBody EmpRequestDTO requestDto, HttpServletResponse response)
 			throws Exception {
