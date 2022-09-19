3338e96030c99ca8237d84ca35d85729c81329b8
도헌's <khs39200@gmail.com>
2022-09-19 17:06:33 +0900

    Feat : test

--- balloon/src/main/java/com/balloon/socket/ChatConfig.java
+++ balloon/src/main/java/com/balloon/socket/ChatConfig.java
@@ -13,7 +13,8 @@ public class ChatConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-		registry.addEndpoint("/chatstart").setAllowedOrigins("http://54.180.148.125").withSockJS();
+		registry.addEndpoint("/chatstart").setAllowedOrigins("http://54.180.148.125", "http://localhost:3000")
+				.withSockJS();
 
 //      .setAllowedOrigins("http://15.164.224.26")
 //				.setAllowedOrigins("http://54.180.148.125")
