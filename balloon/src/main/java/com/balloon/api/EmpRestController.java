a2d5e93b883d3e5ff49cc258e0b395785e211524
도헌's <khs39200@gmail.com>
2022-09-21 16:09:34 +0900

    feat:remove crossorigin

--- balloon/src/main/java/com/balloon/api/EmpRestController.java
+++ balloon/src/main/java/com/balloon/api/EmpRestController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -162,7 +161,7 @@ public class EmpRestController {
 		}
 	}
 
-	@CrossOrigin(origins = { "http://localhost:3000" })
+//	@CrossOrigin(origins = { "http://localhost:3000" })
 	@PutMapping("/update/admin")
 	public void updateEmpByAdmin(@Valid @RequestBody EmpResByAdminDTO empDTO) {
 		try {
@@ -176,7 +175,7 @@ public class EmpRestController {
 		}
 	}
 
-	@CrossOrigin(origins = { "http://localhost:3000" })
+//	@CrossOrigin(origins = { "http://localhost:3000" })
 	@PutMapping("/update/mypage")
 	public void updateEmpByMypage(@Valid @RequestBody EmpResponseDTO empDTO) {
 		try {
