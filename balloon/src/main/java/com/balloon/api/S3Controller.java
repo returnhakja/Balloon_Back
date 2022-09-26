a61313b5c873c8f1220f0873afb1a2399e6bf32c
도헌's <62533556+returnhakja@users.noreply.github.com>
2022-09-26 18:15:14 +0900

    Update S3Controller.java

--- balloon/src/main/java/com/balloon/api/S3Controller.java
+++ balloon/src/main/java/com/balloon/api/S3Controller.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +24,7 @@ public class S3Controller {
 
 	private final S3ServiceImpl s3Svc;
 
-	@PutMapping(value = "/upload/profile/{empId}", consumes = { MediaType.APPLICATION_JSON_VALUE,
+	@PostMapping(value = "/upload/profile/{empId}", consumes = { MediaType.APPLICATION_JSON_VALUE,
 			MediaType.MULTIPART_FORM_DATA_VALUE })
 	public ResponseEntity<String> uploadProfile(@RequestParam(value = "file") MultipartFile multiFile,
 			@PathVariable(value = "empId") String empId) {
